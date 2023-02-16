567dc375534a4b5181ae261173fcb7021976986f
danicaboe <danicaboe14@gmail.com>
2023-02-16 11:40:42 -0800

    testing first commit

---
+++ python/bank.py
@@ -0,0 +1,12 @@
+def main():
+    greeting = input('Greeting: ').strip().lower()
+    if 'hello' in greeting:
+        print('$0')
+    elif greeting[:1] == 'h':
+        print('$20')
+    else:
+        print('$100')
+
+
+
+main()
